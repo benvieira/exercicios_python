d5a5cbca5afe8540a7207d42d321ec4d1ccd10f5
Ben Vieira <ben.vrchads@gmail.com>
2024-07-11 19:36:32 -0300

    respondendo usuário

---
+++ respondendo_ao_usuário.py
@@ -0,0 +1,15 @@
+print('Olá, Mundo')
+nome = input('Digite seu nome:')
+print('É um prazer te conhecer, {}!'.format(nome))
+# nome foi formatado dentro das chaves 
+# input é aquilo que o cliente vai digitar
+
+# int = numero inteiros
+# float = numeros flutuantes / real (Ex:4.5, 0.076, -15.2, 7,0)
+# boll = True ou False 
+# string = 'Palavras' (sempre precisam estar entre aspas)
+
+n1 = int(input ('Digite um valor: '))
+n2 = int(input('Digite um valor:' ))
+soma = n1 + n2
+print('A soma entre {} e {} vale {}.'.format(n1, n2, soma))
