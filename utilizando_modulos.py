0c0e9b9bc48fc358689520c9f0e422f402dc0e47
Ben Vieira <ben.vrchads@gmail.com>
2024-07-21 16:17:17 -0300

    Create utilizando_modulos.py
    programa que lê um número real qualquer e mostre somente sua posição inteira

---
+++ utilizando_modulos.py
@@ -0,0 +1,14 @@
+import math
+n = int(input('Digite um número: '))
+raiz = math.sqrt(n)
+print('A raiz de {} é igual a {}'.format(n, math.floor(raiz)))
+
+# importando a biblioteca 'math' trás funcionalidades extras:
+# ceil (arredondamento para cima)
+# floor (arredondamento para baixo)
+# trunc (eliminar da virgula pra frente)
+# pow (potência)
+# sqrt (raiz quadrada)
+# factorial (calcular fatorial)
+
+#Criar um programa que leia um número real qualquer e mostre na tela somente sua posição inteira.
